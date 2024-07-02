1289bb6f1bcfc4407d3987061a0b448f30c45f7a
krotic <denises.krotic@ya.ru>
2024-07-02 22:07:13 +0500

    заготовка с кодом из другого проекта

--- Play/play.go
+++ Play/play.go
@@ -47,9 +47,75 @@ func InitPlay(
 	}
 }
 
-var timeDur = 20000
+var messChan = make(chan server.Message, 1)
+
+func (p *Play) Queue(ctx context.Context) {
+	for {
+		if len(messChan) == 0 {
+			lastEvent := p.history.GetLast()
+			mes := server.Message{
+				Time: lastEvent.Time + 60,
+				CommandOne: server.Command{
+					Name:  p.CommandOne.Name,
+					Score: lastEvent.CommandOne.Score,
+				},
+				CommandTwo: server.Command{
+					Name:  p.CommandTwo.Name,
+					Score: lastEvent.CommandTwo.Score,
+				},
+				Period: server.Period{
+					TimeInPeriod: lastEvent.Period.TimeInPeriod + 60,
+					Count:        lastEvent.Period.Count,
+					Pause:        lastEvent.Period.Pause,
+				},
+			}
+			if mes.Period.TimeInPeriod >= p.PeriodDuration {
+				mes.Period.Count++
+				mes.Period.TimeStart = time.Now().Unix()
+				mes.Period.TimeInPeriod = 0
+			}
+			err := p.server.Mailing(ctx, &mes)
+			if err != nil {
+				logrus.WithError(err).Errorln("error queuing message")
+				return
+			}
+			p.history.Push(mes)
+		} else {
+			message := <-messChan
+			err := p.server.Mailing(ctx, &message)
+			if err != nil {
+				logrus.WithError(err).Errorln("error queuing message from chan")
+				return
+			}
+		}
+		time.Sleep(time.Second * 1)
+	}
+}
 
 func (p *Play) Playing(ctx context.Context, stop context.CancelFunc) {
+	var lastEvent server.Message
+	if p.history.Len() == 0 {
+		lastEvent = server.Message{
+			Time: 0,
+			CommandOne: server.Command{
+				Name:  p.CommandOne.Name,
+				Score: p.CommandOne.Score,
+			},
+			CommandTwo: server.Command{
+				Name:  p.CommandTwo.Name,
+				Score: p.CommandTwo.Score,
+			},
+			Period: server.Period{
+				TimeStart:    time.Now().Unix(),
+				Count:        1,
+				TimeInPeriod: 0,
+				Pause:        false,
+			},
+		}
+		p.history.Push(lastEvent)
+	}
+	time.Sleep(1 * time.Second)
+	go p.Queue(ctx)
 	for {
 		select {
 		case <-ctx.Done():
@@ -70,33 +136,14 @@ func (p *Play) Playing(ctx context.Context, stop context.CancelFunc) {
 				}).Errorln("Ошибка ввода игровой ситуации")
 				continue
 			}
-			var lastEvent server.Message
-			if p.history.Len() == 0 {
-				lastEvent = server.Message{
-					Time: 0,
-					CommandOne: server.Command{
-						Name:  p.CommandOne.Name,
-						Score: p.CommandOne.Score,
-					},
-					CommandTwo: server.Command{
-						Name:  p.CommandTwo.Name,
-						Score: p.CommandTwo.Score,
-					},
-					Period: server.Period{
-						TimeStart:    time.Now().Unix(),
-						Count:        1,
-						TimeInPeriod: 0,
-					},
-				}
-			} else {
-				le := p.history.GetLast()
-				lastEvent = *le
-			}
+
+			le := p.history.GetLast()
+			lastEvent = *le
 			var event server.Message
 			switch choise {
 			case 1:
 				event = server.Message{
-					Time: lastEvent.Time + (time.Now().Unix() - lastEvent.Time),
+					Time: lastEvent.Time + 60,
 					CommandOne: server.Command{
 						Name:  p.CommandOne.Name,
 						Score: lastEvent.CommandOne.Score + 1,
@@ -107,13 +154,14 @@ func (p *Play) Playing(ctx context.Context, stop context.CancelFunc) {
 					},
 					Period: server.Period{
 						Count:        lastEvent.Period.Count,
-						TimeInPeriod: time.Now().Unix() - lastEvent.Period.TimeStart,
+						TimeInPeriod: lastEvent.Period.TimeInPeriod + 1,
+						Pause:        false,
 					},
 				}
 				break
 			case 2:
 				event = server.Message{
-					Time: lastEvent.Time + (time.Now().Unix() - lastEvent.Time),
+					Time: lastEvent.Time + 1,
 					CommandOne: server.Command{
 						Name:  p.CommandOne.Name,
 						Score: lastEvent.CommandOne.Score,
@@ -124,12 +172,12 @@ func (p *Play) Playing(ctx context.Context, stop context.CancelFunc) {
 					},
 					Period: server.Period{
 						Count:        lastEvent.Period.Count,
-						TimeInPeriod: time.Now().Unix() - lastEvent.Period.TimeStart,
+						TimeInPeriod: lastEvent.Period.TimeInPeriod + 60,
+						Pause:        false,
 					},
 				}
 				break
 			case 3:
-				time.Sleep(10000 * time.Millisecond)
 				event = server.Message{
 					Time: lastEvent.Time,
 					CommandOne: server.Command{
@@ -138,16 +186,19 @@ func (p *Play) Playing(ctx context.Context, stop context.CancelFunc) {
 					},
 					CommandTwo: server.Command{
 						Name:  p.CommandTwo.Name,
-						Score: lastEvent.CommandTwo.Score + 1,
+						Score: lastEvent.CommandTwo.Score,
 					},
 					Period: server.Period{
 						Count:        lastEvent.Period.Count,
 						TimeInPeriod: lastEvent.Period.TimeInPeriod,
+						Pause:        !lastEvent.Period.Pause,
 					},
 				}
 				break
-			default:
+			case 12:
 				<-ctx.Done()
+			default:
+
 			}
 			if event.Period.TimeInPeriod >= p.PeriodDuration {
 				event.Period.Count++
@@ -155,14 +206,7 @@ func (p *Play) Playing(ctx context.Context, stop context.CancelFunc) {
 				event.Period.TimeInPeriod = 0
 			}
 			p.history.Push(event)
-			err = p.server.Mailing(ctx, &event)
-			if err != nil {
-				p.Log.WithFields(logrus.Fields{
-					"error": err,
-				}).Errorln("error mailing")
-				return
-			}
+			messChan <- event
 		}
-
 	}
 }
--- server/period.go
+++ server/period.go
@@ -4,4 +4,5 @@ type Period struct {
 	TimeStart    int64 `json:"time_start"`
 	Count        int   `json:"count"`
 	TimeInPeriod int64 `json:"time_in_period"`
+	Pause        bool  `json:"pause"`
 }
